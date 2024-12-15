a67555bb003a5230122206624b2de1c1acb1cc95
Abizer Nasir <abizern@mac.com>
2024-12-15 04:19:58 +0000

    Day 4 complete

---
+++ Sources/Day04.swift
@@ -0,0 +1,41 @@
+import CryptoKit
+import Foundation
+
+struct Day04: AdventDay, Sendable {
+  let data: String
+  let day = 4
+  let puzzleName: String = "--- Day 4: The Ideal Stocking Stuffer ---"
+
+  init(data: String) {
+    self.data = data
+  }
+
+  func part1() async throws -> Int {
+    generator(data) { hash in
+      hash.hasPrefix("MD5 digest: 00000")
+    }
+  }
+
+  func part2() async throws -> Int {
+    generator(data) { hash in
+      hash.hasPrefix("MD5 digest: 000000")
+    }
+  }
+}
+
+extension Day04 {
+  func generator(_ seed: String, condition: (String) -> Bool) -> Int {
+    var input = 0
+
+    for n in 1 ... 100000000 {
+      input = n
+      let str = seed + String(n)
+      let data = Data(str.utf8)
+      let hash = Insecure.MD5.hash(data: data).description
+      if condition(hash) {
+        break
+      }
+    }
+    return input
+  }
+}
---
+++ Tests/Day04Tests.swift
@@ -0,0 +1,19 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day04 Tests")
+struct Day04Tests {
+  @Suite("Tests on sample inputs")
+  struct SolutionsTests {
+    let day = Day04(data: testInput)
+
+    @Test("Part1 example")
+    func testPart1() async throws {
+      let result = try await day.part1()
+      #expect(result == 609043)
+    }
+  }
+}
+
+private let testInput = "abcdef"
